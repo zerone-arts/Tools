357be0323dd7de489f7cfce7aefa553da123cafc
zerone-arts <zerone-@naver.com>
2024-12-04 16:44:52 +0900

    memo page ui

--- app/memo/page.tsx
+++ app/memo/page.tsx
@@ -1,7 +1,99 @@
 export default function MemoPage() {
   return (
-    <div className="w-full bg-white">
-      <h1>Hellow Memo</h1>
+    <div className="w-full h-screen rounded-xl flex flex-col ">
+      <div className=" w-full h-[170px] flex flex-col bg-gray-100 gap-2 pr-3">
+        <div className="w-full  p-1 pt-5 flex items-center justify-between ">
+          <div className="text-xl pl-10">Memo</div>
+          <div className="flex gap-4">
+            <div className="w-[200px] h-[40px] ">
+              <span className="absolute flex justify-center items-center w-9 h-9 text-gray-400">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5 mt-1"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                  />
+                </svg>
+              </span>
+              <input
+                className="w-full h-[40px] bg-gray-200 rounded-lg pl-8 placeholder:text-gray-400 text-xs focus:outline-none"
+                type="text"
+                placeholder="Search..."
+                // onChange={(e) => console.log(e.target.value)}
+              />
+            </div>
+
+            <button
+              className="w-[120px] border-2 
+          rounded-lg text-sm flex items-center 
+          justify-center border-indigo-400
+          text-indigo-700
+          "
+            >
+              + New Memo
+            </button>
+          </div>
+        </div>
+        <div className="h-full flex justify-end p-1 gap-4">
+          <button
+            className="w-[80px] h-[40px]
+          rounded-lg flex items-center text-xs
+          justify-between bg-gray-200 text-gray-400 px-4
+          "
+          >
+            Sort
+            <span className="text-gray-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={3.0}
+                stroke="currentColor"
+                className="size-3"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </span>
+          </button>
+          <button
+            className="w-[150px] h-[40px] 
+          rounded-lg flex items-center text-xs
+          justify-center bg-gray-200 text-gray-400 
+          "
+          >
+            Group by:
+            <span className="font-light text-black pl-1">All Month</span>
+            <span className="text-gray-600 pl-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={4.0}
+                stroke="currentColor"
+                className="size-3"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </span>
+          </button>
+        </div>
+      </div>
+      <div className="w-full bg-gray-300 h-full border-t-2 border-gray-500 border-opacity-10"></div>
     </div>
   );
 }
--- components/layout/Ui.tsx
+++ components/layout/Ui.tsx
@@ -17,6 +17,7 @@ const category = [
 
 export default function Ui() {
   const [selectMenu, setSelectMenu] = useState("");
+  const [hover, setHover] = useState("");
   const path = usePathname();
 
   useEffect(() => {
@@ -25,14 +26,14 @@ export default function Ui() {
 
   return (
     <div
-      className={`z-10 relative group p-1 w-[80px] hover:w-[200px] h-screen  duration-500 ease-out flex  item-center `}
+      className={`z-10 relative group p-1 w-[80px] hover:w-[200px] h-screen  duration-500 ease-out flex  item-center  `}
     >
-      {/* <div className="absolute h-screen -ml-3">
+      <div className="absolute h-screen -ml-3">
         <div className="mt-3 flex ml-[14px] ">
           <div
             className={`w-[2px] h-9 flex justify-center items-center rounded-sm font-bold duration-300  ${
               selectMenu === "/" ? " opacity-100" : "opacity-0"
-            }`}
+            } `}
           >
             <div className="bg-white w-[2px] h-7 rounded-sm"></div>
           </div>
@@ -84,7 +85,9 @@ export default function Ui() {
           <div
             className={`w-9 h-9 flex justify-center items-center rounded-sm font-bold duration-300  ${
               selectMenu === "/" ? "bg-white text-black" : "text-white"
-            }`}
+            }   ${hover === "/" ? "bg-gray-300 text-black" : "text-white"}`}
+            onMouseOver={() => setHover("/")}
+            onMouseLeave={() => setHover("")}
           >
             <Link href="/">R</Link>
           </div>
@@ -94,7 +97,11 @@ export default function Ui() {
           <li
             className={`w-9 h-9 flex justify-center items-center rounded-sm duration-300 text-white  ${
               selectMenu === "/memo" ? "bg-white text-black" : "text-white"
-            } hover:bg-gray-300 hover:text-black`}
+            }
+            
+            ${hover === "/memo" ? "bg-gray-300 text-black" : "text-white"}`}
+            onMouseOver={() => setHover("/memo")}
+            onMouseLeave={() => setHover("")}
           >
             <Link href="/memo">
               <span>
@@ -116,72 +123,90 @@ export default function Ui() {
             </Link>
           </li>
           <li
-            className={`w-9 h-9 flex justify-center items-center rounded-sm duration-300 text-white 0  ${
+            className={`w-9 h-9 flex justify-center items-center rounded-sm duration-300 text-white  ${
               selectMenu === "/calendar" ? "bg-white text-black" : "text-white"
+            } ${
+              hover === "/calendar" ? "bg-gray-300 text-black" : "text-white"
             }`}
+            onMouseOver={() => setHover("/calendar")}
+            onMouseLeave={() => setHover("")}
           >
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
-                />
-              </svg>
-            </span>
+            <Link href="/calendar">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
+                  />
+                </svg>
+              </span>
+            </Link>
           </li>
           <li
-            className={`w-9 h-9 flex justify-center items-center rounded-sm duration-300 text-white 0  ${
+            className={`w-9 h-9 flex justify-center items-center rounded-sm duration-300 text-white  ${
               selectMenu === "/weather" ? "bg-white text-black" : "text-white"
+            } ${
+              hover === "/weather" ? "bg-gray-300 text-black" : "text-white"
             }`}
+            onMouseOver={() => setHover("/weather")}
+            onMouseLeave={() => setHover("")}
           >
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z"
-                />
-              </svg>
-            </span>
+            <Link href="/weather">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z"
+                  />
+                </svg>
+              </span>
+            </Link>
           </li>
           <li
-            className={`w-9 h-9 flex justify-center items-center rounded-sm duration-300 text-white 0  ${
+            className={`w-9 h-9 flex justify-center items-center rounded-sm duration-300 text-white  ${
               selectMenu === "/calculator"
                 ? "bg-white text-black"
                 : "text-white"
+            } ${
+              hover === "/calculator" ? "bg-gray-300 text-black" : "text-white"
             }`}
+            onMouseOver={() => setHover("/calculator")}
+            onMouseLeave={() => setHover("")}
           >
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z"
-                />
-              </svg>
-            </span>
+            <Link href="/calculator">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z"
+                  />
+                </svg>
+              </span>
+            </Link>
           </li>
         </ul>
 
@@ -189,29 +214,35 @@ export default function Ui() {
           <div
             className={`w-9 h-9 flex justify-center items-center rounded-sm duration-300 text-white 0  ${
               selectMenu === "/setting" ? "bg-white text-black " : "text-white"
+            } ${
+              hover === "/setting" ? "bg-gray-300 text-black" : "text-white"
             }`}
+            onMouseOver={() => setHover("/setting")}
+            onMouseLeave={() => setHover("")}
           >
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-              </svg>
-            </span>
+            <Link href="/setting">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </span>
+            </Link>
           </div>
           <div className="w-8 h-8 flex justify-center items-center rounded-full duration-300 text-white overflow-hidden">
             <Image
@@ -227,76 +258,57 @@ export default function Ui() {
       <div className="absolute ml-[50px] h-screen opacity-0 group-hover:opacity-100 duration-300 text-sm font-light">
         <div className="mt-3 flex ml-[14px] ">
           <div
-            className={`w-[100px] h-9 flex items-center rounded-sm  duration-300  ${
-              selectMenu === "/" ? "text-white" : "text-gray-500"
-            }`}
+            className={`w-[100px] h-9 flex items-center rounded-sm  duration-300 text-gray-200 `}
+            onMouseOver={() => setHover("/")}
+            onMouseLeave={() => setHover("")}
           >
-            Rotion
+            <Link href="/">Rotion</Link>
           </div>
         </div>
 
         <ul className="mt-5  ml-[14px] flex flex-col  justify-center gap-5 ">
           <li
-            className={`w-[100px] h-9 flex  items-center rounded-sm duration-300 text-white  ${
-              selectMenu === "/memo" ? "text-white" : "text-gray-500"
-            }`}
+            className={`w-[100px] h-9 flex  items-center rounded-sm duration-300 text-gray-200 `}
+            onMouseOver={() => setHover("/memo")}
+            onMouseLeave={() => setHover("")}
           >
-            <h1>Memo</h1>
+            <Link href="/memo">Memo</Link>
           </li>
           <li
-            className={`w-[100px] h-9 flex  items-center rounded-sm duration-300 text-white  ${
-              selectMenu === "/calendar" ? "text-white" : "text-gray-500"
-            }`}
+            className={`w-[100px] h-9 flex  items-center rounded-sm duration-300 text-gray-200`}
+            onMouseOver={() => setHover("/calendar")}
+            onMouseLeave={() => setHover("")}
           >
-            <h1>Calendar</h1>
+            <Link href="/calendar">Calendar</Link>
           </li>
           <li
-            className={`w-[100px] h-9 flex  items-center rounded-sm duration-300 text-white ${
-              selectMenu === "/weather" ? "text-white" : "text-gray-500"
-            }`}
+            className={`w-[100px] h-9 flex  items-center rounded-sm duration-300 text-gray-200 `}
+            onMouseOver={() => setHover("/weather")}
+            onMouseLeave={() => setHover("")}
           >
-            <h1>Weather</h1>
+            <Link href="/weather">Weather</Link>
           </li>
           <li
-            className={`w-[100px] h-9 flex items-center rounded-sm duration-300 text-white  ${
-              selectMenu === "/calculator" ? "text-white" : "text-gray-500"
-            }`}
+            className={`w-[100px] h-9 flex items-center rounded-sm duration-300 text-gray-200  `}
+            onMouseOver={() => setHover("/calculator")}
+            onMouseLeave={() => setHover("")}
           >
-            <h1>Calculator</h1>
+            <Link href="/calculator">Calculator</Link>
           </li>
         </ul>
 
-        <div className="absolute  ml-[14px] bottom-[21px] w-full flex flex-col gap-3 ">
+        <div className="absolute  ml-[14px] bottom-[17px] w-full flex flex-col gap-3 ">
           <div
-            className={`w-[100px] h-9 flex items-center rounded-sm duration-300 text-white  ${
-              selectMenu === "/setting" ? "text-white" : "text-gray-500"
-            }`}
+            className={`w-[100px] h-9 flex items-center rounded-sm duration-300 text-gray-200 `}
+            onMouseOver={() => setHover("/setting")}
+            onMouseLeave={() => setHover("")}
           >
-            <p>Setting</p>
+            <Link href="setting">Setting</Link>
           </div>
-          <div className="w-[100px] h-9 flex  items-center  duration-300 text-white">
+          <div className="w-[100px] h-9 flex  items-center  duration-300 text-gray-200">
             ZERONE
           </div>
         </div>
-      </div> */}
-
-      <div className="w-full border-2 flex flex-col justify-between ">
-        <div>
-          <div className="bg-yellow-300 w-full h-[60px]"></div>
-          <div className="bg-blue-300 w-full h-[300px]">
-            <ul>
-              <li className="border-2 border-black h-[50px] flex gap-2">
-                <div className="w-[50px] border-2 border-red-500 flex-shrink-0">
-                  h2
-                </div>
-                <div className="w-[100px] border-2 border-black opacity-0 group-hover:opacity-100 duration-500 ease-out">
-                  test
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="bg-red-300 w-full h-[50px]"></div>
       </div>
     </div>
   );
