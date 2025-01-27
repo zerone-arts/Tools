526f0318d221afd6bd3307fe830c101262b72840
zerone-arts <zerone-@naver.com>
2025-01-27 16:25:41 +0900

    test

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -5,6 +5,11 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 // 클라이언트용 Supabase
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+});
 
 // 서버용 Supabase
