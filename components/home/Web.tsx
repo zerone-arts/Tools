9368f7067d187463f759e857ad975e0df7c2eb5e
zerone-arts <zerone-@naver.com>
2025-01-27 15:27:30 +0900

    Console

--- components/home/Web.tsx
+++ components/home/Web.tsx
@@ -16,7 +16,7 @@ export default function Web({ user }: { user: string | null }) {
   ];
 
   console.log("test");
-  console.log(process.env.NODE_ENV);
+  console.log("NODE_ENV:", process.env.NEXT_PUBLIC_NODE_ENV);
 
   return (
     <div className="w-full h-full  max-md:hidden flex flex-col justify-center items-center gap-0 p-[100px]">
