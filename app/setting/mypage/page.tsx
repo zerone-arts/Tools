80882f44a8d30ba11a1e27d39b85eb2071e90605
zerone-arts <zerone-@naver.com>
2025-01-28 18:03:26 +0900

    AuthContext를 이용해서 초기 로그인 상태와 user상태 관리

--- app/setting/mypage/page.tsx
+++ app/setting/mypage/page.tsx
@@ -43,6 +43,7 @@ export default function MyPage() {
     if (!error) {
       localStorage.setItem("isLoggedIn", "false");
       setLogin(false);
+      console.log(login);
     } else {
       console.error("Error during sign-out:", error);
     }
@@ -63,11 +64,11 @@ export default function MyPage() {
       }
 
       console.log("계정이 성공적으로 삭제되었습니다.");
-
-      await supabase.auth.signOut();
       setDeleteAccountCheck(true);
       localStorage.setItem("isLoggedIn", "false");
       setLogin(false);
+      console.log(login);
+      await supabase.auth.signOut();
     } catch (error) {
       console.error("회원 탈퇴 처리 중 오류:", error);
     }
