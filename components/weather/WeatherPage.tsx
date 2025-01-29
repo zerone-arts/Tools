869f25704ba8566ef71e38a7545facbbf8d776d1
zerone-arts <zerone-@naver.com>
2025-01-29 13:21:27 +0900

    theme test

--- components/weather/WeatherPage.tsx
+++ components/weather/WeatherPage.tsx
@@ -157,7 +157,7 @@ export default function WeatherPage({}) {
   }, []);
 
   return (
-    <div className="relative w-full dark:dark:bg-black/40 dark:text-gray-100 max-sm:h-screen max-sm:overflow-scroll">
+    <div className="relative w-full h-screen dark:text-gray-100 max-sm:h-screen max-sm:overflow-scroll">
       <WeatherUi
         currentLocationHandle={currentLocationHandle}
         setSearchInput={setSearchInput}
